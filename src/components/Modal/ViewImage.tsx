3e29efbb2d7f3a180a2fde5c90c5163c80d9272f
NicholasNeto <nicholasneto@outlook.com>
2021-09-30 22:49:07 -0300

    implement Modal and Image for ModalViewImage component

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -20,4 +20,25 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalBody>
+            <Image
+              maxW={900}
+              maxH={600}
+              boxSize="100px"
+              objectFit="cover"
+              src={imgUrl}
+              alt="Segun Adebayo"
+            />
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+
+  )
 }
